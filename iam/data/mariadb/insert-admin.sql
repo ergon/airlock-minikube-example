299f504a5403b849f74d27c70236541461ed108e
Stefan Dietiker <StefanDietiker@outlook.com>
2020-12-30 12:31:32 +0100

    CHG: iam/data/mariadb - SQL scripts adapted.

--- iam/data/mariadb/insert-admin.sql
+++ iam/data/mariadb/insert-admin.sql
@@ -7,13 +7,11 @@ INSERT INTO medusa_admin (username,
                           surname,
                           roles,
                           pwd_hash,
-                          pwd_next_chg,
                           pwd_chg_enf)
 VALUES ('admin',
         'IAM',
         'Administrator',
         'superadmin',
         'MedusaPwdHistoryAAAARjE2Mzg0fF1jjOhya0ZuJ5OrHXgSXznOBBLl6eJ70PTdFYerSYPwgrlK1uq6UCzc5cvN8t1xtgc+YBQO8UgWBYhzDgpkQGM=',
-        0, /* Password change on next sign-in is disabled */
         0 /* Enforced password change is disabled */
        ) ON DUPLICATE KEY UPDATE username=username;
--- iam/data/mariadb/insert-user.sql
+++ iam/data/mariadb/insert-user.sql
@@ -10,7 +10,6 @@ INSERT INTO `medusa_user` (username,
                            valid,
                            failed_token_counts,
                            pwd_hash,
-                           pwd_next_chg,
                            pwd_chg_enf)
 
 VALUES ('2fa',
@@ -21,6 +20,5 @@ VALUES ('2fa',
         1,
         '{}',
         'MedusaPwdHistoryAAAARjE2Mzg0fEk61Xwi7Q16p0tt9fakb+j9EcLtch4QXhWuGnQzczaRue8NSY6TZIgCGll6+x7ZwT4ujRO3dNvAxBNRM6dZbNY=',
-        0, /* Password change on next sign-in is disabled */
         0 /* Enforced password change is disabled */
        ) ON DUPLICATE KEY UPDATE username=username;
