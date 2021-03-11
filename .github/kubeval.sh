8eb4297d94932e2cafc1324a76e08654366e1112
Roland Weber <roland.weber@ergon.ch>
2021-03-11 10:43:34 +0100

    pass resource directories to kubeeval

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -8,8 +8,5 @@ SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-s
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
-# validate charts
-for RESOURCE_DIR in "init" "example"; do
-  echo "evaluating $RESOURCE_DIR"
-  ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
-done
+# validate yaml files
+./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}" --directories "init,example"
