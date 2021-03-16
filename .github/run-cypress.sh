6275f074d445e8ea4482ad003a701c6556df042f
Roland Weber <roland.weber@ergon.ch>
2021-03-16 16:07:59 +0100

    do not use -ti for cypress docker run

--- .github/run-cypress.sh
+++ .github/run-cypress.sh
@@ -5,4 +5,4 @@ rm -rf .github/build
 mkdir -p .github/build
 cp -r .github/test/* .github/build/
 
-docker run -it -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1
+docker run -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1
