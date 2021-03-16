aa386133e863c9d2f088969e25a8b3a1baa18341
Roland Weber <roland.weber@ergon.ch>
2021-03-16 16:26:07 +0100

    enable access to host.docker.internal from cypress docker container

--- .github/run-cypress.sh
+++ .github/run-cypress.sh
@@ -5,4 +5,4 @@ rm -rf .github/build
 mkdir -p .github/build
 cp -r .github/test/* .github/build/
 
-docker run -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1
+docker run --add-host=host.docker.internal:host-gateway -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1
