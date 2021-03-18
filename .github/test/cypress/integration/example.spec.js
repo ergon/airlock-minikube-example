e3bd0502c5f57bbc2c61cb0d40d9ff6a1b62c5e9
Roland Weber <roland.weber@ergon.ch>
2021-03-18 15:26:45 +0100

    echo service requires authentication now

--- .github/test/cypress/integration/example.spec.js
+++ .github/test/cypress/integration/example.spec.js
@@ -32,6 +32,27 @@ describe('Minikube Example Tests', () => {
   })
 
   it('Access Echo Service', () => {
+    cy.visit('/echo/')
+    //we are now on the iam login page
+    cy.contains('[class="iam-card-header"]', 'Login');
+
+    //login
+    cy.get('#username')
+      .should('be.visible')
+      .type('user')
+    cy.get('#password')
+      .should('be.visible')
+      .type('password')
+
+    cy.get('button[type="submit"]')
+      .should('be.visible')
+      .click()
+
+    // verify that we are on the portal
+    cy.contains('[class="page-title"]', 'Portal');
+
+    //find the Echo icon,
+    cy.contains('Echo');
 
     //access the echo service and verify that it went through the microgateway
     // by looking for env cookie content
