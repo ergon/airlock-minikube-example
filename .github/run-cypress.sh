c148d332e3a1ec88f5269f957bd3fc90ee5e85d6
Roland Weber <roland.weber@ergon.ch>
2021-03-22 09:25:01 +0100

    use chrome browser

--- .github/run-cypress.sh
+++ .github/run-cypress.sh
@@ -6,7 +6,7 @@ mkdir -p .github/build
 cp -r .github/test/* .github/build/
 
 if [ "$(uname -s)" == "Linux" ]; then
-  docker run --rm --add-host=host.docker.internal:host-gateway -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1
+  docker run --rm --add-host=host.docker.internal:host-gateway -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1 run --browser chrome
 else
-  docker run --rm -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1
+  docker run --rm -v $PWD/.github/build:/e2e -w /e2e cypress/included:6.0.1 run --browser chrome
 fi
